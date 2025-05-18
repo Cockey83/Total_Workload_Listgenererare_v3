6792328c1c5fae91253f597e12c4014315701505
Cockey83 <cockey_83@hotmail.com>
2025-05-18 23:07:04 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import TotalWorkloadListgenererare from './TotalWorkloadListgenererare';
+
+function App() {
+    return <TotalWorkloadListgenererare />;
+}
+
+export default App;
